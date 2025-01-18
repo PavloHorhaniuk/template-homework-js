99a50e694323b90c1c1a81191ea4ee437e2bfa0c
PavloHorhaniuk <medvejuy123@gmail.com>
2025-01-18 13:02:29 +0100

    next

---
+++ practice-js/js/lesson-8.js
@@ -0,0 +1,12 @@
+//? Перетворити масив:
+const arrayOriginal = ["мango-10", "Kiwi-2", "Monkong-2", "Singu-2", "Mango-1", "Ajax-1", "Poly-1", "singu-20"];
+//? на масив arrayNew:
+//todo: ["Mango-1", "Ajax-1", "Poly-1", "Kiwi-2", "Monkong-2", "Singu-2"];
+//? не змінюючи значення елементів та використовуючи методи:
+//? push(), pop(), unshift(), shift(),
+//? slice(), splice(), concat() за власним бажанням!
+//! Код виконаного завдання
+let arrayNew = [];
+console.log("arrayOriginal:", arrayOriginal);
+console.log("arrayNew:", arrayNew);
+console.log(". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .");
