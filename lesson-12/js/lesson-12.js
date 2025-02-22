b6f6c988318ffc16cda0aaa7ac94ac6b02f6876f
PavloHorhaniuk <medvejuy123@gmail.com>
2025-02-22 18:33:21 +0100

    дз+

---
+++ lesson-12/js/lesson-12.js
@@ -0,0 +1,177 @@
+//todo [1]
+console.log(
+    "%c [1] ",
+    "color: yellow; background-color: #2274A5",
+);
+//? Напиши скрипт, який, для об'єкта user, послідовно:
+//? - додає поле mood зі значенням 'happy'
+//? - замінює значення hobby на 'skydiving'
+//? - замінює значення premium на false
+const user = {
+    name: 'Mango',
+    age: 20,
+    hobby: 'html',
+    premium: true,
+};
+//! Код виконаного завдання
+
+console.log("--------------------------------------------------");
+
+
+//todo [2]
+console.log(
+    "%c [2] ",
+    "color: yellow; background-color: #2274A5",
+);
+//? Напиши функцію countProps(obj),
+//? яка рахує кількість властивостей в об'єкті.
+//? Функція повертає число — кількість властивостей.
+//! Код виконаного завдання
+const countProps = function (obj) {
+    //! твій код
+};
+//! Викличи функції для перевірки працездатності твоєї реалізації.
+console.log(countProps({})); //! 0
+console.log(countProps({ name: 'Mango', age: 2 })); //! 2
+console.log("--------------------------------------------------");
+
+
+//todo [3]
+console.log(
+    "%c [3] ",
+    "color: yellow; background-color: #2274A5",
+);
+//? Напиши функцію findBestEmployee(employees), 
+//? яка приймає об'єкт співробітників 
+//? і повертає ім'я найпродуктивнішого (який виконав більше всіх задач). 
+//? Співробітники і кількість виконаних завдань містяться 
+//? як властивості об'єкта в форматі "ім'я":"кількість задач".
+//! Код виконаного завдання
+const findBestEmployee = function (employees) {
+    //! твій код
+};
+
+//! Викличи функції для перевірки працездатності твоєї реалізації.
+console.log(
+    findBestEmployee({
+        ann: 29,
+        david: 35,
+        helen: 1,
+        lorence: 99,
+    }),
+); //! lorence
+
+console.log(
+    findBestEmployee({
+        poly: 12,
+        mango: 17,
+        ajax: 4,
+    }),
+); //! mango
+
+console.log(
+    findBestEmployee({
+        lux: 147,
+        david: 21,
+        kiwi: 19,
+        chelsy: 38,
+    }),
+); //! lux
+console.log("--------------------------------------------------");
+
+
+//todo [4]
+console.log(
+    "%c [4] ",
+    "color: yellow; background-color: #2274A5",
+);
+//? Напиши функцію countTotalSalary(employees) приймаючу об'єкт зарплат. 
+//? Функція рахує загальну суму зарплати працівників і повертає її. 
+//? Кожне поле об'єкта, переданого в функцію, має вигляд "ім'я":"зарплата".
+//! Код виконаного завдання
+const countTotalSalary = function (employees) {
+    //! твій код
+};
+
+//! Викличи функції для перевірки працездатності твоєї реалізації.
+console.log(countTotalSalary({})); //! 0
+
+console.log(
+    countTotalSalary({
+        mango: 100,
+        poly: 150,
+        alfred: 80,
+    }),
+); //! 330
+
+console.log(
+    countTotalSalary({
+        kiwi: 200,
+        lux: 50,
+        chelsy: 150,
+    }),
+); //! 400
+console.log("--------------------------------------------------");
+
+
+//todo [5]
+console.log(
+    "%c [5] ",
+    "color: yellow; background-color: #2274A5",
+);
+//? Напиши функцію getAllPropValues(arr, prop), 
+//? яка отримує масив об'єктів і ім'я властивості. 
+//? Повертає масив значень певної властивості prop 
+//? з кожного об'єкта в масиві.
+//! Код виконаного завдання
+const products = [
+    { name: 'Радар', price: 1300, quantity: 4 },
+    { name: 'Сканер', price: 2700, quantity: 3 },
+    { name: 'Дроїд', price: 400, quantity: 7 },
+    { name: 'Захоплення', price: 1200, quantity: 2 },
+];
+
+const getAllPropValues = function (arr, prop) {
+    //! твій код
+};
+
+//! Викличи функції для перевірки працездатності твоєї реалізації.
+console.log(getAllPropValues(products, 'name')); //! ['Радар', 'Сканер', 'Дроїд', 'Захоплення']
+console.log(getAllPropValues(products, 'quantity')); //! [4, 3, 7, 2]
+console.log(getAllPropValues(products, 'category')); //! []
+console.log("--------------------------------------------------");
+
+
+
+//todo [6]
+console.log(
+    "%c [6] ",
+    "color: yellow; background-color: #2274A5",
+);
+//? Напиши функцію calculateTotalPrice(allProdcuts, productName), 
+//? яка отримує масив об'єктів та ім'я продукту (значення властивості name). 
+//? Повертає загальну вартість продукту (ціна * кількість).
+//? Викличи функції для перевірки працездатності твоєї реалізації.
+//! Код виконаного завдання
+const calculateTotalPrice = function (allProdcuts, productName) {
+    //! твій код
+};
+
+//! Викличи функції для перевірки працездатності твоєї реалізації.
+console.log(calculateTotalPrice(products, 'Радар')); //! 5200
+console.log(calculateTotalPrice(products, 'Дроїд')); //! 2800
+console.log("--------------------------------------------------");
+
+
+
+//todo [7] - додаткове
+console.log(
+    "%c [7] - додаткове ",
+    "color: yellow; background-color: #2274A5",
+);
+//? Напиши сценарій керування особистим кабінетом інтернет-банку. 
+//? Є об'єкт account в якому необхідно реалізувати методи 
+//? для роботи з балансом та історією транзакцій.
+//! Код виконаного завдання
+
+console.log("--------------------------------------------------");
---
+++ practice-js/js/lesson-13.js
@@ -0,0 +1,180 @@
+// // books
+// const books = {
+//     count: 3,
+//     list: [
+//       {
+//         title: 'The Great Gatsby',
+//         author: 'F. Scott Fitzgerald',
+//         year: 1925
+//       },
+//       {
+//         title: 'To Kill a Mockingbird',
+//         author: 'Harper Lee',
+//         year: 1960
+//       },
+//       {
+//         title: '1984',
+//         author: 'George Orwell',
+//         year: 1949
+//       }
+//     ],
+//   };
+//   //! Код виконаного завдання
+// const{
+//     list: [
+//         {title1: title1,author: author1,year: year1},
+//         {title2: title2,author2: author2,year2: year2},
+//         {title3: title3,author3: author3,year: year3},
+//     ]
+// } = books;
+
+
+
+// console.log("count: ", count);
+
+// console.log("title1: ", title1);  
+// console.log("author1: ", author1);  
+// console.log("year1: ", year1);  
+
+// console.log("title2: ", title2);  
+// console.log("author2: ", author2);  
+// console.log("year2: ", year2);
+
+// console.log("title3: ", title3);  
+// console.log("author3: ", author3);  
+// console.log("year3: ", year3);
+
+
+
+//todo [1]
+console.log(
+    "%c [1] ",
+    "color: yellow; background-color: #2274A5",
+);
+//? ✴️ Створіть об'єкт "bankAccount" з трьома властивостями:
+//?  "ownerName", "accountNumber", "balance"
+//? та додайте їм будь-яких логічних значень значень.
+//? ✴️ За допомогою додавання властивостей
+//? додайте до об'єкту метод "deposit",
+//? який, використовуючи методи взаємодії з користувачем,
+//? додає гроші на рахунок,
+//? та виводить повідомлення про залишок на рахунку
+//? дублюючи його в консоль.
+//? ✴️ За допомогою додавання властивостей,
+//? додайте до об'єкту метод "withdraw",
+//? який, використовуючи методи взаємодії з користувачем,
+//? дозволяє знімати гроші з рахунку,
+//? якщо на рахунку достатньо коштів,
+//? та виводить повідомлення про залишок на рахунку,
+//? дублюючи його в консоль.
+//? Якщо на рахунку не вистачає коштів, то він виводить повідомлення:
+//? "⛔️ Не достатньо коштів на вашому рахунку!",
+//? дублюючи його в консоль.
+//? ✳️ За допомогою виклика методу "deposit" об'єкта "bankAccount" додай кошти на рахунок.
+//? ✳️ За допомогою виклика методу "withdraw" об'єкта "bankAccount" зніми кошти з рахунку.
+//! Код виконаного завдання
+
+// const bankAccount = {
+//     ownerName: "Jack Nicholson",
+//     accountNumber: 1111222233334444,
+//     balance: 10000,
+// };
+// console.log("Our object before: ", bankAccount);
+
+// bankAccount.deposit = function (money) {
+//     console.log('deposit');
+
+//     this.balance += money;
+
+//     console.lod("Our balance: ", this.balance);
+// };
+
+// bankAccount.withdraw = function () {
+//     console.log('withdraw');
+
+//     this.balance -= money;
+//     if (this.balance < money) {
+//         console.log("⛔️ Не достатньо коштів на вашому рахунку!");
+//         return
+
+//     };
+
+//     console.lod("Our balance: ", this.balance);
+// }
+// console.log("Our object after: ", bankAccount);
+
+// console.log(bankAccount.deposit(1000));
+
+// console.log(bankAccount.withdraw(11000));
+
+// console.log("--------------------------------------------------");
+
+
+
+//todo [6]
+console.log(
+    "%c [6] ",
+    "color: yellow; background-color: #2274A5",
+);
+//? ✴️ Напиши сценарій керування особистим кабінетом інтернет-банку.
+//? Є об'єкт account в якому необхідно реалізувати методи 
+//? для роботи з балансом та історією транзакцій.
+//? ✳️ Типів транзацкій всього два.
+//? Можна покласти або зняти гроші з рахунку.
+const Transaction = {
+    DEPOSIT: 'deposit',
+    WITHDRAW: 'withdraw',
+};
+
+//todo: Кожна транзакція - це об'єкт з властивостями: id, type і amount
+
+const account = {
+    //todo: Поточний баланс рахунку
+    balance: 0,
+    //todo:  Історія транзакцій
+    transactions: [],
+
+
+    //todo:  Метод створює і повертає об'єкт транзакції.
+    //todo:  Приймає суму і тип транзакції.
+    createTransaction(amount, type) {
+        const transactionsObject = { 
+            id
+            type
+            amount
+        }
+
+        console.log(transactionsObject)
+        return transactionsObject
+    },
+    
+
+    //todo:  Метод відповідає за додавання суми до балансу.
+    //todo:  Приймає суму танзакції.
+    //todo:  Викликає createTransaction для створення об'єкта транзакції
+    //todo:  після чого додає його в історію транзакцій
+    deposit(amount) { },
+
+    
+    //todo:  Метод відповідає за зняття суми з балансу.
+    //todo:  Приймає суму танзакції.
+    //todo:  Викликає createTransaction для створення об'єкта транзакції
+    //todo:  після чого додає його в історію транзакцій.
+    //todo:  Якщо amount більше, ніж поточний баланс, виводь повідомлення
+    //todo:  про те, що зняття такої суми не можливо, недостатньо коштів.
+    withdraw(amount) { },
+
+    //todo:  Метод повертає поточний баланс
+    getBalance() { },
+
+    //todo:  Метод шукає і повертає об'єкт транзакції по id
+    getTransactionDetails(id) { },
+
+    //todo:  Метод повертає кількість коштів
+    //todo:  певного типу транзакції з усієї історії транзакцій
+    getTransactionTotal(type) { },
+};
+//! Код виконаного завдання
+
+console.log("--------------------------------------------------");
+
